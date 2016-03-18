44fe23cb307c87fc79fa25b1a3b50050540eac58
Kliriklara <klara@geckoboard.com>
2016-03-18 21:03:55 +0000

    Add a Navbar

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,9 @@
 import React, {
   AppRegistry,
   Component,
+  NavigatorIOS,
 } from 'react-native';
+import Cards from './src/cards';
 
 const MOCKED_CARDS_DATA = [
   { text: "You can't get blood out of a stone" },
@@ -14,14 +16,39 @@ const MOCKED_CARDS_DATA = [
   { text: "That's the way the cookie crumbles" }
 ];
 
-import Cards from './src/cards';
-
 class Tidioms extends Component {
+  constructor() {
+    super();
+    this.onRightButtonPress = this.onRightButtonPress.bind(this);
+  }
+
+  onRightButtonPress() {
+    this.refs.nav.push({
+      title: 'Settings',
+      component: Cards,
+      passProps: { cards: MOCKED_CARDS_DATA },
+    })
+  }
+
   render() {
     return (
-      <Cards cards={MOCKED_CARDS_DATA} style={{flex: 1}} />
+      <NavigatorIOS
+        ref='nav'
+        barTintColor={BAR_TINT_COLOR}
+        itemWrapperStyle={{ flex:1 }}
+        style={{ flex:1 }}
+        initialRoute={{
+          title: 'Tidioms Cards',
+          component: Cards,
+          passProps: { cards: MOCKED_CARDS_DATA },
+          rightButtonTitle: 'Settings',
+          onRightButtonPress: this.onRightButtonPress,
+        }}
+      />
     );
   }
 }
+
+const BAR_TINT_COLOR = '#BDBA62';
 
 AppRegistry.registerComponent('tidioms', () => Tidioms);
